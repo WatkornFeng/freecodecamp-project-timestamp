8d2deccfd07d433b4ab01a4f1a3ed09186b1d9f2
Watcharakorn <watcharakornk2022@gmail.com>
2023-12-27 21:51:46 +0700

    create /api/:date

--- index.js
+++ index.js
@@ -2,31 +2,55 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
+app.get("/api/:date", (req, res) => {
+  let dateParams = req.params.date;
 
+  //check if dateParams's not include non-number and it's not date string.
+  const isUnix = /^\d+$/.test(dateParams);
 
+  if (isUnix) {
+    const convertToUnix = parseInt(dateParams);
+    const date = new Date(convertToUnix);
+    return res.json({
+      unix: convertToUnix,
+      utc: date.toUTCString(),
+    });
+  }
 
+  const dateToUnix = Date.parse(dateParams);
+  if (dateToUnix) {
+    const date = new Date(dateToUnix);
+    return res.json({
+      unix: dateToUnix,
+      utc: date.toUTCString(),
+    });
+  }
+  return res.json({
+    error: "Invalid Date",
+  });
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
